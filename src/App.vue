254096e4c89468f311e5dbb5bbc5af537b40ff07
PengDaJin <1036550354@qq.com>
2020-11-28 17:10:36 +0800

    删除单元格

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="nav">
-    <van-cell-group>
-  <van-cell title="单元格" value="内容" />
-  <van-cell title="单元格" value="内容" />
-</van-cell-group> 
+   
   </div>
   <router-view/>
 </template>
